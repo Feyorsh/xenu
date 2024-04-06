15a4f92e585e94657f5c4c825b237802bd58d643
George Huebner <george@feyor.sh>
2024-04-06 18:22:38 -0500

    build with impure xcode and /usr/bin/swiftc

--- flake.nix
+++ flake.nix
@@ -10,25 +10,15 @@
       url = "github:nix-community/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    # fyshpkgs = {
-    #   url = "git+file:///Users/ghuebner/Personal/fyshpkgs";
-	  #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
   };
 
   outputs = { self, nixpkgs, nixos-generators }:
     let system = "aarch64-darwin";
         gdb_overlay = final: prev': {
           gdb = prev'.gdb.overrideAttrs (prev: {
-            configurePlatforms = [];
+            configurePlatforms = [ ];
 
             configureFlags = with prev.pkgs.lib; [
-              # Set the program prefix to the current targetPrefix.
-              # This ensures that the prefix always conforms to
-              # nixpkgs' expectations instead of relying on the build
-              # system which only receives `config` which is merely a
-              # subset of the platform description.
-              #"--program-prefix=${prev.targetPrefix}"
               "--program-prefix="
 
               "--disable-werror"
@@ -42,91 +32,16 @@
               "--with-system-gdbinit=/etc/gdb/gdbinit"
               "--with-system-gdbinit-dir=/etc/gdb/gdbinit.d"
 
-              #"--with-gmp=${prev.gmp.dev}"
-              #"--with-mpfr=${prev.mpfr.dev}"
-              #"--with-expat" "--with-libexpat-prefix=${prev.expat.dev}"
               "--with-auto-load-safe-path=/"
             ];
-            # iggy = pkgs.writeText "ignore-errors.py" ''
-            #     class IgnoreErrorsCommand (gdb.Command):
-            #         """Execute a single command, ignoring all errors.
-            #     Only one-line commands are supported.
-            #     This is primarily useful in scripts."""
-
-            #         def __init__ (self):
-            #             super (IgnoreErrorsCommand, self).__init__ ("ignore-errors",
-            #                                                         gdb.COMMAND_OBSCURE,
-            #                                                         # FIXME...
-            #                                                         gdb.COMPLETE_COMMAND)
-
-            #         def invoke (self, arg, from_tty):
-            #             try:
-            #                 gdb.execute (arg, from_tty)
-            #             except:
-            #                 pass
-
-            #     IgnoreErrorsCommand ()
-            # '';
-            # postInstall = prev.postInstall + ''
-            #   cp $iggy $out/share/gdb/python/ignore-errors.py
-            # '';
             meta.platforms = prev.meta.platforms ++ [ "aarch64-darwin" ];
           });
-          # pwndbg = prev'.pwndbg.overrideAttrs (prev: {
-          #   binPath = pkgs.lib.makeBinPath ( with pkgs; [
-          #     python3.pkgs.pwntools
-          #     python3.pkgs.ropper
-          #     python3.pkgs.ropgadget
-          #   ]);
-          #   meta.broken = false;
-          # });
-          # gef = prev'.gef.overrideAttrs (prev: let
-          #   pythonPath = with prev'.python3.pkgs; makePythonPath [
-          #     keystone-engine
-          #     unicorn
-          #     capstone
-          #     final.python3.pkgs.ropper
-          #   ];
-          # in {
-          #   installPhase = ''
-          #     mkdir -p $out/share/gef
-          #     cp gef.py $out/share/gef
-          #     makeWrapper ${pkgs.gdb}/bin/gdb $out/bin/gef \
-          #     --add-flags "-q -x $out/share/gef/gef.py" \
-          #     --set NIX_PYTHONPATH ${pythonPath} \
-          #     --prefix PATH : ${prev'.lib.makeBinPath [
-          #         prev'.python3
-          #         #bintools-unwrapped # for readelf
-          #         final.python3.pkgs.pyelftools
-          #         prev'.file
-          #         prev'.ps
-          #     ]}
-          # '';
-          # });
-          # python3 = prev'.python3.override {
-          #   packageOverrides = python-self: python-super: {
-          #     ropper = python-super.ropper.overrideAttrs (oldAttrs: {
-          #       meta.broken = false;
-          #     });
-          #     pyelftools = python-super.pyelftools.overrideAttrs (oldAttrs: {
-          #       # nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [
-          #       #   prev'.makeBinaryWrapper
-          #       # ];
-          #       # installPhase = ''
-          #       #   makeBinaryWrapper $out/bin/readelf.py $out/bin/readelf
-          #       # '';
-          #       postInstall = ''
-          #         cp $out/bin/readelf.py $out/bin/readelf
-          #       '';
-          #     });
-          #   };
-          # };
         };
 
         dpkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
-          overlays = [ gdb_overlay ];
+          # overlays = [ gdb_overlay ];
         };
     in {
     nixosModules.base = {pkgs, ...}: {
@@ -156,10 +71,7 @@
         file
         patchelf
         fd
-        # pkgsCross.gnu64.glibc
       ];
-
-      # virtualisation.vmVariant.virtualisation.fileSystems = pkgs.lib.mkForce { };
     };
     nixosModules.vm = {pkgs, ...}: {
       # no gui
@@ -177,7 +89,7 @@
         "kernel.yama.ptrace_scope" = 0;
       };
 
-      # tmpfs too small for building e.g. glibc, could change if dependencies weren't dynamic
+      # tmpfs too small for building e.g. glibc, could change if you don't care about using nix-shell in guest
       virtualisation.vmVariant.virtualisation.writableStoreUseTmpfs = false;
 
       boot.initrd.availableKernelModules = [ "virtiofs" ];
@@ -202,39 +114,13 @@
       pwny-vmlinux = self.nixosConfigurations.linuxVM.config.system.build.kernel;
       pwny-initrd = self.nixosConfigurations.linuxVM.config.system.build.initialRamdisk;
       fake-qemu = self.nixosConfigurations.linuxVM.config.system.build.vm;
-      # xenu = let
-      #   # buildSymlinks = dpkgs.runCommand "macvim-build-symlinks" {} ''
-      #   #   mkdir -p $out/bin
-      #   #   ln -s /usr/bin/xcrun /usr/bin/xcodebuild /usr/bin/tiffutil /usr/bin/qlmanage $out/bin
-      #   # '';
-      # in dpkgs.stdenv.mkDerivation {
-      #         name = "xenu";
-      #         src = ./xenu;
-      #         # sandboxProfile = ''
-      #         #   (allow file-read* file-write* process-exec mach-lookup)
-      #         #   ; block homebrew dependencies
-      #         #   (deny file-read* file-write* process-exec mach-lookup (subpath "/usr/local") (with no-log))
-      #         # '';
-      #         meta = with dpkgs.lib; {
-      #           description = "Command line interface to Apple Virtualization";
-      #           homepage = "https://github.com/Feyorsh/xenu";
-      #           license = licenses.gpl3;
-      #           maintainers = [ ];
-      #           platforms = platforms.darwin;
-      #           # required if we do the macvim route
-      #           # hydraPlatforms = [];
-      #         };
-      # };
+      xenu = dpkgs.callPackage ./xenu.nix {};
     };
     devShells.${system}.default = with dpkgs; mkShell {
       packages = [
-        # swift
-        darwin.xcode_15_1
-        # utm
-        gdb
+        self.packages.${system}.xenu
+        # gdb
       ];
     };
   };
 }
-
-Build input file cannot be found: '/Users/ghuebner/Downloads/xenu/LinuxVirtualMachine/LinuxVirtualMachine.entitlements'. Did you forget to declare this file as an output of a script phase or custom build rule which produces it?
